2eadf67de200b85270291dfef8718c8343c66752
Jesse Rao <jesse.rao@liferay.com>
2015-09-17 18:56:15 -0400

    LRDOCS-1864 Fix bnd.bnd in guestbook.api, specify title for Guestbook portlet, add language properties

--- develop/tutorials/code/guestbook/guestbook.web/src/main/java/guestbook/web/GuestbookPortlet.java
+++ develop/tutorials/code/guestbook/guestbook.web/src/main/java/guestbook/web/GuestbookPortlet.java
@@ -1,9 +1,16 @@
 package guestbook.web;
 
+import java.io.IOException;
+
 import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
+import com.liferay.docs.guestbook.service.GuestbookLocalService;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 
 @Component(
@@ -19,5 +26,35 @@ import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 	service = Portlet.class
 )
 public class GuestbookPortlet extends MVCPortlet {
+	
+	/*
+	public GuestbookLocalService getGuestbookLocalService() {
+		return _guestbookLocalService;
+	}
+	
+	@Reference
+	public void setGuestbookLocalService(
+			GuestbookLocalService guestbookLocalService) {
+
+		_guestbookLocalService = guestbookLocalService;
+	}
+	*/
+	
+	@Override
+	public void render(RenderRequest request, RenderResponse response)
+			throws IOException, PortletException {
+		
+		/*
+		int guestbooksCount = getGuestbookLocalService().getGuestbooksCount();
+		
+		System.out.println("Guestbooks count: " + guestbooksCount);
+		*/
+
+		super.render(request, response);
+	}
+	
+	/*
+	private GuestbookLocalService _guestbookLocalService;
+	*/
 
 }
