3aeb239a809c36e51ca52d2d04f3b8b32416079d
Jesse Rao <jesse.rao@liferay.com>
2015-11-05 18:20:34 -0500

    LRDOCS-1864 Update Guestbook portlet to show guestbooks count

--- develop/learning-paths/mvc/code/com.liferay.docs.guestbook/com.liferay.docs.guestbook.web/src/main/java/com/liferay/docs/guestbook/web/GuestbookPortlet.java
+++ develop/learning-paths/mvc/code/com.liferay.docs.guestbook/com.liferay.docs.guestbook.web/src/main/java/com/liferay/docs/guestbook/web/GuestbookPortlet.java
@@ -1,9 +1,16 @@
 package com.liferay.docs.guestbook.web;
 
+import java.io.IOException;
+
 import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
+import com.liferay.docs.guestbook.service.GuestbookLocalService;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 
 @Component(
@@ -19,5 +26,33 @@ import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 	service = Portlet.class
 )
 public class GuestbookPortlet extends MVCPortlet {
+	
+	@Override
+	public void render(RenderRequest request, RenderResponse response)
+			throws IOException, PortletException {
+		
+		GuestbookLocalService guestbookLocalService = getGuestbookLocalService();
+		
+		int guestbooksCount = guestbookLocalService.getGuestbooksCount();
+
+		//set service bean
+		request.setAttribute("GUESTBOOKS_COUNT", guestbooksCount);
+
+		super.render(request, response);
+	}
+	
+	
+	public GuestbookLocalService getGuestbookLocalService() {
+
+		return _guestbookLocalService;
+	}
+
+	@Reference
+	public void setGuestbookLocalService(GuestbookLocalService guestbookLocalService) {
+
+		this._guestbookLocalService = guestbookLocalService;
+	}
+	
+	private GuestbookLocalService _guestbookLocalService;
 
 }
--- develop/learning-paths/mvc/code/com.liferay.docs.guestbook/com.liferay.docs.guestbook.web/src/main/resources/META-INF/resources/view.jsp
+++ develop/learning-paths/mvc/code/com.liferay.docs.guestbook/com.liferay.docs.guestbook.web/src/main/resources/META-INF/resources/view.jsp
@@ -3,3 +3,11 @@
 <p>
 	<b><liferay-ui:message key="guestbook_GuestbookPortlet.caption"/></b>
 </p>
+
+<%
+	int guestbooksCount = GetterUtil.getInteger(renderRequest.getAttribute("GUESTBOOKS_COUNT"));
+%>
+
+<p>
+	There are <%= guestbooksCount %> guestbooks!
+</p>
